905acbff485468fb9a2a1fb21e634ba9037e4461
azenlabsllc <contact@azenlabs.com>
2025-04-17 17:19:29 -0500

    remove sticky header and add favicon

--- js/index.js
+++ js/index.js
@@ -35,11 +35,11 @@
   window.onscroll = function () {
     // ===  Sticky Navbar
     const header = document.querySelector(".navbar");
-    if (window.pageYOffset >= 100) {
-      header.classList.add("sticky-navbar");
-    } else {
-      header.classList.remove("sticky-navbar");
-    }
+//    if (window.pageYOffset >= 100) {
+//      header.classList.add("sticky-navbar");
+//    } else {
+//      header.classList.remove("sticky-navbar");
+//    }
 
     // ===  show or hide the back-top-top button
     const backToTop = document.querySelector(".back-to-top");
